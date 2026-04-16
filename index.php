afd6dc6d48f2b05eebee3b2f68f94a97fd06af1a
tobiasstupak <tobiasstupak@gmail.com>
2026-04-16 20:45:07 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h2>Prihlasenie</h2>
+
+<form method="POST">
+    <input type="text" name="meno" placeholder="meno"><br><br>
+    <input type="password" name="heslo" placeholder="heslo"><br><br>
+    <button type="submit" name="login">Prihlasit sa</button>
+</form>
+
+</body>
+</html>
+
 <?php
 include "db.php";
 
@@ -15,21 +32,3 @@ if (isset($_POST['login'])) {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-
-<h2>Prihlasenie</h2>
-
-<form method="POST">
-    <input type="text" name="meno" placeholder="meno"><br><br>
-    <input type="password" name="heslo" placeholder="heslo"><br><br>
-    <button type="submit" name="login">Prihlasit sa</button>
-</form>
-
-</body>
-</html>
