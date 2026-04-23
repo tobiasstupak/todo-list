3d2ec37d8c8d1f9c929efb1f0e1c2cdea022e07c
tobiasstupak <tobiasstupak@gmail.com>
2026-04-23 10:00:14 +0200

    funkcna registracia
    Updated registration form to redirect on successful registration and fixed placeholder text.

--- registracia.php
+++ registracia.php
@@ -1,25 +1,7 @@
-<?php
-include "db.php";
-
-if (isset($_POST['register'])) {
-    $meno = $_POST['meno'];
-    $heslo = $_POST['heslo'];
-
-    if ($meno != "" && $heslo != "") {
-        $sql = "INSERT INTO users (meno, heslo) VALUES ('$meno', '$heslo')";
-        mysqli_query($conn, $sql);
-
-        echo "Uspešná registrácia";
-    } else {
-        echo "Vyplň všetky údaje";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
-    <head>
-    </head>
+<head>
+</head>
 <body>
 
 <h2>Registracia</h2>
@@ -27,8 +9,29 @@ if (isset($_POST['register'])) {
 <form method="POST">
     <input type="text" name="meno" placeholder="meno"><br><br>
     <input type="password" name="heslo" placeholder="heslo"><br><br>
-    <button type="submit" name="register">Registrovat</button>
+    <button type="submit" name="registracia">Registrovat</button>
 </form>
 
 </body>
 </html>
+
+<?php
+include "db.php";
+
+if (isset($_POST['registracia'])) {
+    $meno = $_POST['meno'];
+    $heslo = $_POST['heslo'];
+
+    if ($meno != "" && $heslo != "") {
+        $sql = "INSERT INTO users (meno, heslo) VALUES ('$meno', '$heslo')";
+        mysqli_query($conn, $sql);
+
+        header("Location: todo.php");
+        exit;
+    } else {
+        echo "Vypln vsetky udaje";
+    }
+}
+?>
+
+
