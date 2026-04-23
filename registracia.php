86e5e2a78d9bf777590f902e6871468249c480a6
tobiasstupak <tobiasstupak@gmail.com>
2026-04-23 09:01:52 +0200

    Pridanie formulara na registráciu používateľov
    Added a registration form with fields for name and password.

--- registracia.php
+++ registracia.php
@@ -15,3 +15,20 @@ if (isset($_POST['register'])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+<body>
+
+<h2>Registracia</h2>
+
+<form method="POST">
+    <input type="text" name="meno" placeholder="meno"><br><br>
+    <input type="password" name="heslo" placeholder="heslo"><br><br>
+    <button type="submit" name="register">Registrovat</button>
+</form>
+
+</body>
+</html>
