9a45637b336429ded52c3de3a3953a9055c46630
Samuel-Sosta <samkostrela1@gmail.com>
2026-04-23 08:25:33 +0200

    Improve database connection error handling
    Updated error message to include specific connection error details and set UTF-8 character set for the database connection.

--- db.php
+++ db.php
@@ -2,6 +2,8 @@
 $conn = mysqli_connect("localhost", "root", "root", "db_todo");
 
 if (!$conn) {
-    die("Chyba pripojenia");
+    die("Chyba pripojenia k databaze: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8");
 ?>
