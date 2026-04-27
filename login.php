4e5357c4a89f2a00c6732bdf4e01f3501e317aea
Samuel-Sosta <samkostrela1@gmail.com>
2026-04-27 07:49:34 +0200

    oprava taskov

--- login.php
+++ login.php
@@ -10,6 +10,9 @@ if (isset($_POST['login'])) {
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['meno'] = $user['meno'];
         header("Location: todo.php");
         exit;
     } else {
